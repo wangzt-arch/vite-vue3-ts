a883d31e65f3665fec512c16f998421aa8f82042
DESKTOP-G0KKLD0\rsc <15731416521@163.com>
2022-09-01 11:20:35 +0800

    chore: 第一次提交

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+interface Props {
+  msg: string
+}
+
+const props = defineProps<Props>()
+const message = props.msg
+const msgClick = () => {
+  console.log(message);
+}
+onMounted(() => {
+  console.log("初始化");
+})
+
+</script>
+
+<template>
+  <h1 class="green">{{ msg }}</h1>
+  {{ message }}
+</template>
+
+<style scoped>
+</style>
